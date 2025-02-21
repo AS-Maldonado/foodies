7c368fee7c5930a053f65bf73fa017f92a312dee
Augusto <asmaldonado02@gmail.com>
2025-02-20 22:18:03 -0300

    feat: finalizando  aplicacao

---
+++ app/meals/[meal]/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <div className="error">
+      <h1>Failed to create a meal!</h1>
+      <p>Please try again later...</p>
+    </div>
+  );
+}
